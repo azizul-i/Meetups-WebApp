154eb8969aee08c8535d7554ddf8d113ccd5021b
Azizul <misl000@aucklanduni.ac.nz>
2020-09-07 18:05:01 +1200

    Created skeletal front-end

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import { Header, Icon, List } from "semantic-ui-react"
+import "./App.css"
+import "semantic-ui-css/semantic.min.css"
+import axios from "axios"
+
+class App extends Component {
+  state = {
+    values: [],
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="map marker alternate" />
+          <Header.Content>Meetups</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    )
+  }
+}
+
+export default App
